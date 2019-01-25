a5c471a1e9a6ee9eb599d03d8eef5a1bfbd4412b
unknown <mail@k-roth.com>
2019-01-25 22:04:49 +0100

    Querys und Daten implementierung für Vorstand / Figuren / Dorffasnet

--- src/pages/vorstand.js
+++ src/pages/vorstand.js
@@ -5,13 +5,22 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 export const query = graphql`
-  query VorstandPageQuery {
-    page: allSanityVorstand {
+  query vorstandPageQuery {
+    elfer: allSanityPerson(filter: { rollen: { eq: "elferat" } }) {
       edges {
         node {
           id
-          title
           name
+          rollen
+        }
+      }
+    }
+    rest: allSanityPerson(filter: { rollen: { ne: "elferat" } }) {
+      edges {
+        node {
+          id
+          name
+          rollen
         }
       }
     }
@@ -20,7 +29,6 @@ export const query = graphql`
 
 const vorstandPage = ({ data }) => {
   console.log(data)
-
   return (
     <Layout>
       <SEO
@@ -28,8 +36,22 @@ const vorstandPage = ({ data }) => {
         keywords={[`vornstand`, `präsident`, `vizepräsident`]}
       />
       <h2>Der Vorstand</h2>
-      <p>{data.page.edges.map(e => e.node.title)}</p>
-      <p>{data.page.edges.map(e => e.node.name)}</p>
+
+      {data.rest.edges.map(e => (
+        <div key={`${e.node.id}`}>
+          <h3 key={`${e.node.id}-rollen`}>{e.node.rollen}</h3>
+          <p key={`${e.node.id}-name`}>{e.node.name}</p>
+        </div>
+      ))}
+
+      <div>
+        <h3>{data.elfer.edges[0].node.rollen}</h3>
+        {data.elfer.edges.map(e => (
+          <ul key={`${e.node.id}`}>
+            <li key={`${e.node.id}-name`}>{e.node.name}</li>
+          </ul>
+        ))}
+      </div>
     </Layout>
   )
 }

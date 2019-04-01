c19c84860a03b2d25e95b93b7e88f6beea5fc3f3
Roth <rtt66126@r1stt02.tuttlingen.cn.rt.iiru.de>
2019-04-01 09:36:35 +0200

    btn-cta Link fix

--- src/components/Btn-Cta/btnCta.js
+++ src/components/Btn-Cta/btnCta.js
@@ -5,10 +5,11 @@ import { Link } from 'gatsby'
 export default class BtnCta extends Component {
   render() {
     return (
-      <Link className={styles.btnText} to={this.props.link}>
-        {' '}
-        <button className={styles.btn}>{this.props.children}</button>
-      </Link>
+      <button className={styles.btn}>
+        <Link className={styles.btnText} to={this.props.link}>
+          {this.props.children}
+        </Link>
+      </button>
     )
   }
 }

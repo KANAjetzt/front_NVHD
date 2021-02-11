8bab86812e0840bf098b2df36ea65c2dc6303387
KANAjetzt <kai@kana.jetzt>
2021-02-11 21:14:57 +0100

    ✔ usable

--- src/components/curtain/curtain.js
+++ src/components/curtain/curtain.js
@@ -1,13 +1,84 @@
 import React, { useEffect } from 'react'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+import Countdown from 'react-countdown'
 import styles from './curtain.module.scss'
+import ImageCompVereinsfiguren from '../image-comp-Vereinsfiguren/imageCompVereinsfiguren'
 
 const Curtain = () => {
+  const { width, height } = useWindowSize()
+
+  // const countDownDate = new Date(2021, 1, 12, 20)
+  const countDownDate = Date.now() + 1000 * 2
+
+  const checkCountDown = (
+    curtainLeft,
+    curtainRight,
+    content,
+    ground,
+    bg,
+    confetti,
+    container,
+    ytVideo
+  ) => {
+    setInterval(() => {
+      // Check countdown date
+      if (Date.now() < countDownDate) return
+
+      // Start curtain animation
+      curtainLeft.style.animationPlayState = 'running'
+      curtainRight.style.animationPlayState = 'running'
+
+      // Fade in Ground
+      ground.style.animationPlayState = 'running'
+
+      // Fade in dark bg
+      bg.style.animationPlayState = 'running'
+
+      // Fade out confetti
+      confetti.style.animationPlayState = 'running'
+
+      // fade out container content
+      content.style.animationPlayState = 'running'
+
+      // fade down lights
+      container.style.animationPlayState = 'running'
+
+      // fade up ytVideo
+      ytVideo.style.animationPlayState = 'running'
+
+      // change z-index of ytVideo
+      setTimeout(() => {
+        ytVideo.style.zIndex = 200
+      }, 1000 * 8)
+    }, 1000)
+  }
+
   useEffect(() => {
+    const curtainLeft = document.querySelector(`.${styles.left.split(' ')[0]}`)
+    const curtainRight = document.querySelector(
+      `.${styles.right.split(' ')[0]}`
+    )
+    const content = document.querySelector(`.${styles.content}`)
+    const container = document.querySelector(`.${styles.container}`)
+    const ground = document.querySelector(`.${styles.ground}`)
+    const bg = document.querySelector(`.${styles.bg}`)
+    const confetti = document.querySelector(`.${styles.main} canvas`)
+    const ytVideo = document.querySelector(`.${styles.ytVideo}`)
+
+    checkCountDown(
+      curtainLeft,
+      curtainRight,
+      content,
+      ground,
+      bg,
+      confetti,
+      container,
+      ytVideo
+    )
+
     // DOM Elemente als Variable nutzbar mache
-    console.log(styles)
-    console.log(`.${styles.container__glow1}`)
     const GLOW1 = document.querySelector(`.${styles.container__glow1}`)
-    console.log(GLOW1)
     const GLOW2 = document.querySelector(`.${styles.container__glow2}`)
 
     // Größe des Fensters als Variable nutzbar machen
@@ -19,7 +90,6 @@ const Curtain = () => {
       // Mouse Koordinaten definieren
       const MOUSE_X = event.clientX
       const MOUSE_Y = event.clientY
-      console.log(MOUSE_Y + ' : ' + MOUSE_X)
 
       // Differenz berechnen
       const DELTA_X1 = Math.round(((MOUSE_X / WIDTH) * 150) / 3, 0)
@@ -40,17 +110,32 @@ const Curtain = () => {
 
   return (
     <section className={styles.main}>
+      <Confetti width={width} height={height} />
       <div className={styles.curtainOverlay}>
         <div className={styles.curtainContainer}>
           <div className={styles.left}></div>
           <div className={styles.right}></div>
         </div>
+        <div className={styles.ground}></div>
       </div>
-
       <div className={styles.container}>
+        <div className={styles.content}>
+          <ImageCompVereinsfiguren />
+          <Countdown zeroPadTime={2} date={countDownDate} />
+        </div>
         <div className={styles.container__glow1}></div>
         <div className={styles.container__glow2}></div>
       </div>
+      <iframe
+        className={styles.ytVideo}
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/Moqq5DcntCs"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <div className={styles.bg} />
     </section>
   )
 }
